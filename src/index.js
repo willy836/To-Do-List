41ee520d86646c82ee407f1883e8993a89925538
willy836 <wilsonkimiri836@gmail.com>
2022-12-06 22:16:28 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
 import './style.css';
 
 const data = [
-    {
-        description: `go shopping`,
-        completed: false,
-        index: 1
-    },
-    {
-        description: `watch football`,
-        completed: true,
-        index: 2
-    },
-    {
-        description: `complete today's project`,
-        completed: false,
-        index: 3
-    },
-    {
-        description: `go for a walk`,
-        completed: true,
-        index: 4
-    }
+  {
+    description: 'go shopping',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'watch football',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'complete today\'s project',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'go for a walk',
+    completed: true,
+    index: 4,
+  },
 ];
 
 const listContainer = document.querySelector('.todo-list');
 
-window.addEventListener('DOMContentLoaded', ()=> {
-    listString = data.map((list)=> {
-        return `<article>
+window.addEventListener('DOMContentLoaded', () => {
+  const listString = data.map((list) => `<article>
         <div class="inp-p">
             <input type="checkbox">
             <p>${list.description}</p>
         </div>
         <i class="fa-solid fa-ellipsis-vertical"></i>
-    </article>`
-    })
+    </article>`)
     .join('');
-    listContainer.innerHTML = listString;
-    
+  listContainer.innerHTML = listString;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: path.resolve(__dirname, './src/index.js')
-    },
-    devServer: {
-        static: './dist',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-           template: path.resolve(__dirname, './src/index.html'),
-        }),
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-      module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-}
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
