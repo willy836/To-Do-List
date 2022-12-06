7e10337f16465f836a6ee52bb610b52ec52ed377
willy836 <wilsonkimiri836@gmail.com>
2022-12-06 22:06:48 +0300

    Render dynamically created lists on the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import './style.css';
 
-const title = document.createElement('div');
-title.innerText = 'My Title';
+const data = [
+    {
+        description: `go shopping`,
+        completed: false,
+        index: 1
+    },
+    {
+        description: `watch football`,
+        completed: true,
+        index: 2
+    },
+    {
+        description: `complete today's project`,
+        completed: false,
+        index: 3
+    },
+    {
+        description: `go for a walk`,
+        completed: true,
+        index: 4
+    }
+];
 
-const body = document.querySelector('body');
-body.appendChild(title);
+const listContainer = document.querySelector('.todo-list');
+
+window.addEventListener('DOMContentLoaded', ()=> {
+    listString = data.map((list)=> {
+        return `<article>
+        <div class="inp-p">
+            <input type="checkbox">
+            <p>${list.description}</p>
+        </div>
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+    </article>`
+    })
+    .join('');
+    listContainer.innerHTML = listString;
+    
+});
